0d3cc0b6e451f8b557321bccfb6175d722258d30
viktorlv30 <viktor.lv.30@gmail.com>
2017-12-17 22:12:30 +0200

    implemented video translation in chromium without adapters and STUN

--- client-ts/UserMedia/UserMedia.ts
+++ client-ts/UserMedia/UserMedia.ts
@@ -5,14 +5,21 @@
 import { IUserMedia } from "./IUserMedia";
 
 export class UserMedia implements IUserMedia {
-
     private _navigator: Navigator;
+    private _startButton: HTMLButtonElement | null;
+    private _localVideoElemId: string;
+    private _localVideoArea: HTMLVideoElement | null;
+    private _remoteVideoElemId: string;
+    private _remoteVideoArea: HTMLVideoElement | null;
+
     // is started video or audio recording/streaming. after press start button
     isStarted: boolean;
 
     constructor() {
         this._navigator = this.GetNavigator();
         this.isStarted = false;
+        this._localVideoElemId = 'localVideo';
+        this._remoteVideoElemId = 'remoteVideo';
     }
 
     Start(constraints: MediaStreamConstraints): void {
@@ -51,7 +58,14 @@ export class UserMedia implements IUserMedia {
         console.log(`Start stream handler!`);
         this.isStarted = true;
         console.log(stream);
-
+        let videoTracks = stream.getVideoTracks();
+        console.log(`Using media device:`, videoTracks[0].label);
+        let videoTag: HTMLVideoElement | null = this.GetVideoArea(this._localVideoElemId);
+        if (videoTag) {
+            videoTag.srcObject = stream;
+        } else {
+            throw `Tag with id '${this._localVideoElemId}' doesn't exist!`;
+        }
     }
 
     ErrorHandler(error: MediaStreamError): void {
@@ -59,8 +73,28 @@ export class UserMedia implements IUserMedia {
         this.Stop();
     }
 
-    GetStartButton(id: string): HTMLElement | null {
-        return document.getElementById(id);
+    GetStartButton(id: string): HTMLButtonElement | null {
+        this._startButton = <HTMLButtonElement>document.getElementById(id);
+        return this._startButton;
+    }
+
+    /**
+     * GetVideoArea - 'local' or 'remote'
+     * @param areaType  - for show local or remote stream
+     * @returns HTMLVideoElement video
+     */
+    GetVideoArea(areaType: string): HTMLVideoElement | null {
+        let element: HTMLVideoElement | null;
+        if (areaType === this._localVideoElemId) {
+            this._localVideoArea = <HTMLVideoElement>document.getElementById(areaType);
+            element = this._localVideoArea;
+        } else if (areaType === this._remoteVideoElemId) {
+            this._remoteVideoArea = <HTMLVideoElement>document.getElementById(areaType);
+            element = this._remoteVideoArea;
+        } else {
+            throw `Incorrect identifier for local or remote video html tag. It must be either ${this._localVideoElemId} or ${this._remoteVideoElemId}`;
+        }
+        return element;
     }
 
     private GetNavigator(): Navigator {
